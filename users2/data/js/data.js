6f7d96e466d5b79e259775636e20bff809c3712c
Attila <quamdiuetiam@hotmail.com>
2020-07-08 13:30:16 +0200

    modified3

--- users2/data/js/data.js
+++ users2/data/js/data.js
@@ -1,5 +1,8 @@
 let pack = new Object;
 let along = 1;
+// along = 1 : ne töltse be a felhasználókhoz a set/del gombokat
+// along = 0 : töltse be
+let button = String();
 
 getData = function (url) {
     //szervertől lekéri az adatokat
@@ -29,7 +32,7 @@ function createNewElement(name, attribute, value) {
 }
 
 let loadData = function (along) {
-    //betölti az adatokat az első három cellába
+    //felhasználónként készít egy új sort és betölti az adatokat az első három cellájába
     for (let i = 0; i < pack.length; i++) {
         let row = createNewElement("tr");
         let area = document.querySelector("#t-users");
@@ -48,45 +51,81 @@ let loadData = function (along) {
     }
 };
 
-function mainButtonClick() {
-    //betölti vagy eltávolítja az adatmódosító gombokat onclick-re
+let mainButton = document.querySelector("#mainButton");
+mainButton.addEventListener("click", mainButtonClick);
 
+
+function mainButtonClick() {
+    button = "main";
+    //betölti vagy eltávolítja az adatmódosító gombokat onclick-re
     let allowRun = Boolean(true);
 
-    if (document.querySelector("#t-users tr").childElementCount == 3) {
+    //gombok betöltése után az allowRun = false lesz,
+    //ha true marad, akkor a gombok eltávolítását várjuk
 
+    if (document.querySelector("#t-users tr").childElementCount == 3) {
+        //csak három cella van az első felhasználónál, ezért mindegyik felhasználónál
+        //betölti a mellékgombokat
+        allowRun = false;
         for (let i = 0; i < pack.length; i++) {
-            allowRun = false;
+
             loadButtonGroup(index = i);
             loadSetDelButton(index = i);
         };
+
+        //láthatóvá teszi az új felhasználó fejlécét
         mainButton.innerHTML = "Beállítások (vissza)"
+        let newUser = document.querySelector("#new-user");
+        newUser.style.display = "";
+
+        //létrehozza és betölti az új felhasználó input mezőit
+        let row = document.querySelector("#new-user-input");
+        setUser(row, btnValue = pack.length + 1);
     };
 
-    if (allowRun === true) {
+    if (allowRun == true) {
+        mainButton.innerHTML = "Beállítások";
+        let newUser = document.querySelector("#new-user");
+        newUser.style.display = "none";
+
+        //eltávolítja az új felhasználó input mezőit
+        let row = document.querySelector("#new-user-input");
+        itemCount=row.childElementCount;
+        for (let i=0; i<itemCount; i++){
+            row.removeChild(row.lastChild)
+        }
+
+
         for (let i = 0; i < pack.length; i++) {
-            if (i == 0) {
-                let placeForNewHead = document.querySelector("thead tr");
-                placeForNewHead.removeChild(placeForNewHead.lastChild);
-            };
             let area = document.querySelectorAll("#t-users tr")[i];
             area.removeChild(area.lastChild);
             let mainButton = document.querySelector("#mainButton")
-            mainButton.innerHTML = "Beállítások"
+            mainButton.innerHTML = "Beállítások";
+            actions = document.querySelector("#actions");
+            actions.style.display = "none";
         }
+        //takeBackNewUserRow();
     }
+
 }
 
-function loadButtonGroup() {
-    //betölti az 'action' fejlécet
-    if (index == 0) {
-        let placeForNewHead = document.querySelector("thead tr");
-        newHead = createNewElement("th");
-        newHead.innerHTML = "Actions";
-        placeForNewHead.appendChild(newHead);
-    };
+/*function displayNewUserRow() {
+    place = document.querySelector("#new-user-text");
+    place.innerHTML = "Új felhasználó";
+};
 
-    //betölti a gombcsoportokat
+function takeBackNewUserRow() {
+    place = document.querySelector("#new-user-text");
+    place.innerHTML = "";
+}
+*/
+
+
+function loadButtonGroup() {
+
+    //betölti a gombcsoportokat a láthatóvá váló actions fejléc alá
+    actions = document.querySelector("#actions");
+    actions.style.display = "";
     let col = createNewElement("td");
     let area = document.querySelectorAll("#t-users tr")[index];
     area.appendChild(col);
@@ -133,38 +172,41 @@ function delUser(event, el) {
         .catch(er => console.log(er))
 };
 
-function clearTable() {
-    // törli a #t-users táblázat sorait
-    table = document.querySelector("#t-users");
+function clearRow(elem) {
+    // törli a #t-users táblázat inputokkal ellátott sorát
+    /*let elemValue = elem.value;
+    elemValue = parseInt(elemValue);
+    let row = elem.parentElement.parentElement.parentElement;
+    let table = document.querySelector("#t-users");
+    table.removeChild(row);
+    console.log(elemValue);
+    let w=document.querySelector("#t-users").children(elemValue);*/
+
+    // törli az összes sor 
+    let table = document.querySelector("#t-users");
     rowItems = table.childElementCount;
     rowItems = parseInt(rowItems);
     for (let i = 0; i < rowItems; i++) {
         table.removeChild(table.lastElementChild);
     };
-    tr = document.querySelector("thead tr");
-    tr.removeChild(tr.lastElementChild);
-    //visszaállítja a főgombot a disabled állapotból
     let mainButton = document.querySelector("#mainButton");
     mainButton.removeAttribute("disabled");
 
 }
 
 function changeUserData(event, btn) {
-    //btn = 'this' in onclick
-    btnValue = btn["value"];
+    button = "minor";
+    //btn = 'this' in onclick; btnValue a gomb value-ja ami a felhasználó id-je
+    let btnValue = btn["value"];
     btnValue = parseInt(btnValue);
-    row = btn.parentElement.parentElement.parentElement;
-    for (let i = 0; i < 3; i++) { row.removeChild(row.children[3 - i]) };
+    let row = btn.parentElement.parentElement.parentElement;
+    //a teljes sort kiürítjük, maga az üres sor megmarad
+    //row.removeChild(row.lastElementChild);
+    for (let i = 0; i < 4; i++) { row.removeChild(row.lastElementChild) };
+    //for (let i = 0; i < 5; i++) { row.removeChild(row.children[3 - i]) };
 
     let mainbutton = document.querySelector("#mainButton");
     mainbutton.setAttribute("disabled", "");
-
-    createSetDelButtons();
-};
-
-
-
-function createSetDelButtons() {
 
     btnGroupList = document.querySelectorAll(".btn-group");
     for (let k = 0; k < btnGroupList.length; k++) {
@@ -172,35 +214,121 @@ function createSetDelButtons() {
         btnGroupList[k].children[1].setAttribute("disabled", "")
     };
 
-    col = createNewElement("td");
-    row.appendChild(col);
-    nameInput = createNewElement("input", {
-        'type': 'text', 'class': 'form-control',
-        'placeholder': `${pack[btnValue - 1]["name"]}`
-    });
-    col.appendChild(nameInput);
-    col = createNewElement("td");
-    row.appendChild(col);
-    emailInput = createNewElement("input", {
-        'type': 'email', 'class': 'form-control',
-        'placeholder': `${pack[btnValue - 1]["email"]}`
-    });
-    col.appendChild(emailInput);
-
-    col = createNewElement("td");
-    row.appendChild(col);
-    btnGroup = createNewElement("div", { 'class': 'btn-group btn-group-sm', 'role': 'group', 'aria-label': 'Basic example' });
-    col.appendChild(btnGroup);
-    sendButton = createNewElement('button', { 'type': 'button', 'class': 'btn btn-primary' });
-    btnGroup.appendChild(sendButton);
-    sendButton.innerHTML = '<i class="fa fa-wrench" aria-hidden="true"></i>';
-    cancelButton = createNewElement('button', { 'type': 'button', 'class': 'btn btn-secondary', 'onclick': 'clearTable(),loadData(along=0)' });
-    btnGroup.appendChild(cancelButton);
-    cancelButton.innerHTML = '<i class="fa fa-ban" aria-hidden="true"></i>';
+    setUser(row, btnValue);
 };
 
-let mainButton = document.querySelector("#mainButton");
-mainButton.addEventListener("click", mainButtonClick);
+function setUser(row, btnValue) {
+
+    for (k in { 'id': {}, 'name': {}, 'email': {}, 'btnGroup': {} }) {
+        col = createNewElement("td");
+        row.appendChild(col);
+        if (k == 'id') {
+            col.setAttribute('class', 'pt-3'); col.setAttribute('name', k);
+            col.innerHTML = btnValue;
+        };
+        if (k == 'name' && button == 'minor') {
+            nameInput = createNewElement("input", {
+                'name': k,
+                'type': 'text', 'class': 'form-control',
+                'placeholder': `${pack[btnValue - 1]["name"]}`
+            });
+            col.appendChild(nameInput);
+        }
+        if ((k == 'name') && (button == 'main')) {
+            nameInput = createNewElement("input", {
+                'name': k,
+                'type': 'text', 'class': 'form-control',
+                'placeholder': 'név'
+            });
+            col.appendChild(nameInput);
+        }
+        if (k == 'email' && button == 'minor') {
+            emailInput = createNewElement("input", {
+                'name': k,
+                'type': 'text', 'class': 'form-control',
+                'placeholder': `${pack[btnValue - 1]["email"]}`
+            });
+            col.appendChild(emailInput);
+        }
+        if (k == 'email' && button == 'main') {
+            emailInput = createNewElement("input", {
+                'name': k,
+                'type': 'text', 'class': 'form-control',
+                'placeholder': 'email'
+            });
+            col.appendChild(emailInput);
+        }
+        if (k == 'btnGroup') {
+            btnGroup = createNewElement("div", {
+                'name': k, 'class': 'btn-group btn-group-sm pt-1',
+                'role': 'group', 'aria-label': 'Basic example'
+            });
+            col.appendChild(btnGroup);
+
+            sendButton = createNewElement('button', { 'type': 'button', 'class': 'btn btn-primary' });
+            btnGroup.appendChild(sendButton);
+            sendButton.innerHTML = '<i class="fa fa-wrench" aria-hidden="true"></i>';
+
+            cancelButton = createNewElement('button', {
+                'type': 'button', 'class': 'btn btn-secondary', 'value': `${btnValue}`,
+                'onclick': 'clearRow(this),loadData(along=0)'
+            });
+            btnGroup.appendChild(cancelButton);
+            cancelButton.innerHTML = '<i class="fa fa-ban" aria-hidden="true"></i>';
+        }
+    }
+};
+
+/*function createSendCancelButton(btnGroup) {
+
+}
+
+function createNewUser() {
+    for (k in { 'id': {}, 'name': {}, 'email': {}, }){
+
+    }
+}
+*/
+
+/*  A KORÁBBI, REFAKTORÁLÁS NÉLKÜL
+function createSetDelButtons(btnValue) {
+col = createNewElement("td", { 'style': 'text-align: center', 'class': 'pt-3' });
+row.appendChild(col);
+col.innerHTML = btnValue;
+
+col = createNewElement("td");
+row.appendChild(col);
+nameInput = createNewElement("input", {
+    'type': 'text', 'class': 'form-control',
+    'placeholder': `${pack[btnValue - 1]["name"]}`
+});
+col.appendChild(nameInput);
+
+col = createNewElement("td");
+row.appendChild(col);
+emailInput = createNewElement("input", {
+    'type': 'email', 'class': 'form-control',
+    'placeholder': `${pack[btnValue - 1]["email"]}`
+});
+col.appendChild(emailInput);
+
+col = createNewElement("td");
+row.appendChild(col);
+btnGroup = createNewElement("div", { 'class': 'btn-group btn-group-sm', 'role': 'group', 'aria-label': 'Basic example' });
+col.appendChild(btnGroup);
+
+sendButton = createNewElement('button', { 'type': 'button', 'class': 'btn btn-primary' });
+btnGroup.appendChild(sendButton);
+sendButton.innerHTML = '<i class="fa fa-wrench" aria-hidden="true"></i>';
+
+cancelButton = createNewElement('button', { 'type': 'button', 'class': 'btn btn-secondary', 'onclick': 'clearTable(),loadData(along=0)' });
+btnGroup.appendChild(cancelButton);
+cancelButton.innerHTML = '<i class="fa fa-ban" aria-hidden="true"></i>';
+};
+*/
+
+
+
 
 
 
